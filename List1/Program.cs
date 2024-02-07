ff380d94fb0e7ce2fc450f81d2804df2d30234c4
baronvonpurr <robinhood.svensson@hotmail.com>
2024-02-07 15:12:29 +0100

    Add project files.

---
+++ List1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namn = new List<string>();
+            bool loop = true;
+            while (loop == true)
+            {
+                Console.WriteLine("Skriv in ett namn eller skriv blankt för att fortsätta");
+                namn.Add(Console.ReadLine());
+                if (namn.Last() == "")
+                {
+                    namn.Sort();
+                    namn.Reverse();
+                    for (int i = 0; i < namn.Count; i++)
+                    {
+                        Console.WriteLine(namn[i]);
+                        loop = false;
+                    }
+                }
+            }
+        }
+    }
+}
